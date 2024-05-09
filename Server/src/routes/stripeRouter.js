811375a80ad50f175c3bec117e9a9a7ce6e75ce1
FranciscoPer <panchopertile@hotmail.com>
2024-05-09 16:49:14 -0300

    (add/checkout)

--- Server/src/routes/stripeRouter.js
+++ Server/src/routes/stripeRouter.js
@@ -2,10 +2,12 @@ const { Router } = require ("express");
 const { createProductHandler } = require('../handlers/stripe/createProductHandler');
 const { createPlanHandler} = require('../handlers/stripe/createPlanHandler')
 const {createSubscriptionHandler} = require ('../handlers/stripe/createSubscriptionHandler')
+const {createCheckoutSessionHandler} = require('../handlers/stripe/checkOutSessionHandler')
 const stripeRouter = Router();
 
 stripeRouter.post('/products', createProductHandler);
 stripeRouter.post('/plans', createPlanHandler);
 stripeRouter.post('/subscriptions', createSubscriptionHandler);
+stripeRouter.post('/create-checkout-session', createCheckoutSessionHandler);
 
 module.exports = stripeRouter;
