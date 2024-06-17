6511be88d4da77eedeeda300f1267c635bd26207
FranciscoPer <panchopertile@hotmail.com>
2024-06-17 12:40:58 -0300

    deploy

--- Server/src/db.js
+++ Server/src/db.js
@@ -1,11 +1,12 @@
 const { Sequelize } = require("sequelize");
 const fs = require('fs');
 const path = require('path');
+require('dotenv').config();
 
-
-const sequelize = new Sequelize('upnomada', 'postgres', '1234', {
-  host: 'localhost',
-  dialect: 'postgres'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres',
+  port: process.env.DB_PORT || 5432
 });
 
 const basename = path.basename(__filename);
