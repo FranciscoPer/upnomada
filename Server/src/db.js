b6146f1d557cd7069f5dad54f1004740a2457d5d
FranciscoPer <panchopertile@hotmail.com>
2024-06-18 19:05:40 -0300

    Db funcionando

--- Server/src/db.js
+++ Server/src/db.js
@@ -3,11 +3,16 @@ const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: 'postgres',
-  port: process.env.DB_PORT 
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT || 5432,
+  }
+);
 
 const basename = path.basename(__filename);
 const modelDefiners = [
